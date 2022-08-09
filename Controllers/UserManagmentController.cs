67733f093369f8e9589090e0157aeb10bcb7d1d2
Jaier Calero <martinezolban2015@gmail.com>
2022-08-08 18:30:50 -0600

    Proyecto Iniciado

---
+++ Controllers/UserManagmentController.cs
@@ -0,0 +1,137 @@
+﻿using AuthServiceDavivienda.Context;
+using AuthServiceDavivienda.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AuthServiceDavivienda.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserManagmentController : ControllerBase
+    {
+        private readonly userDbContext _context;
+
+        public UserManagmentController(userDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("create-user")]
+        public object  RegistrarUsuario(Employed empleado)
+        {
+            try
+            {
+                empleado.CreatedAt = DateTime.Now;
+                empleado.UpdateAt  = DateTime.Now;
+                empleado.State = 1;
+                empleado.EncryptedPass = Seguridad.EncodeHash(empleado.EncryptedPass);
+                empleado.CurrentIp = HttpContext.Connection.RemoteIpAddress.ToString();
+
+                if (_context.Employeds == null)
+                {
+                    return NotFound(new{ msj = "Modelo no Enccontrado"});
+                }
+
+                _context.Employeds.Add(empleado);
+                _context.SaveChanges();
+
+            }catch(Exception ex){
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(new { msj = "Registro Ingresado con Exito" });
+        }
+
+        [HttpPost("assign-role")]
+        public object AsignarRole(UserRole userRole)
+        {
+            try
+            {
+                if (_context.UserRoles == null )
+                {
+                    return BadRequest(new { msj = "Modelo no Encontrado" });
+                }
+                if (!_context.Employeds.Any(x => x.Identification.Equals(userRole.UserIdentity))) return NotFound();
+                if (!_context.Roles.Any(x => x.Id.Equals(userRole.RoleId))) return NotFound();
+                
+                userRole.UpdatedAt = DateTime.Now;
+                userRole.CreatedAt = DateTime.Now;
+
+                _context.Add(userRole);
+                _context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(new
+            {
+                msj = "Registro Ingresado Correctamente",
+                UserRole = userRole
+            });
+
+        }
+
+        [HttpGet("Login")]
+        public object Login([Required][FromHeader] string Authorization)
+        {
+            string base64 = Authorization.Replace("Basic ", "");
+            var EncodeTextBytes = Convert.FromBase64String(base64);
+            var textoPlano = Encoding.UTF8.GetString(EncodeTextBytes);
+            var arreglo = textoPlano.Split(":");
+            var email = arreglo[0];
+            var pass = arreglo[1];
+
+            if (!_context.Employeds.Any(x => x.Email.Equals(email))) return NotFound(new {msj= "Usuario o Contraseña Incorrecta" });
+            Employed empleado = _context.Employeds.Where(x => x.Email.Equals(email)).First();
+
+            if (!(empleado.EncryptedPass == Seguridad.EncodeHash(pass))) return Unauthorized(new {msj= "Usuario o Contraseña Incorrecta" });
+
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray()) + DateTime.Now;
+            token = Seguridad.EncodeHash( Regex.Replace(token, ' '.ToString(), string.Empty).Trim());
+            UserToken userTokenDB = new UserToken();
+
+            try
+            {
+                if (_context.UserTokens.Any(x => x.UserIdentity.Equals(empleado.Identification)))
+                {
+                    userTokenDB = _context.UserTokens.Where(x => x.UserIdentity.Equals(empleado.Identification)).First();
+                    userTokenDB.CurrentToken = DateTime.Now;
+                    userTokenDB.LastToken = userTokenDB.CurrentToken;
+                    userTokenDB.UserIdentity = empleado.Identification;
+                    _context.Entry(userTokenDB).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    userTokenDB.CurrentToken = DateTime.Now;
+                    userTokenDB.LastToken = DateTime.Now;
+                    userTokenDB.HashToken = token;
+                    userTokenDB.UserIdentity = empleado.Identification;
+                    _context.UserTokens.Add(userTokenDB).State = EntityState.Added;
+                    _context.SaveChanges();
+                }
+            }catch(Exception ex)
+            {
+                 return Ok(new{
+                    msj = ex.Message,
+                    userToken = userTokenDB
+                 });
+            }
+
+
+            return Ok(new
+            {
+                msj = "Bienvenido al sistema",
+                userToken = userTokenDB
+            });
+
+        }
+
+    }
+}
---
+++ Seguridad.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthServiceDavivienda
+{
+    public class Seguridad
+    {
+        //Variables para cifrado de la contraseña
+        private static string palapaso = "WS_CAP";
+        private static string valorSalt = "WS_CAP_2022";
+        private static string encrip = "SHA1";
+        private static string vector = "1234567891234567";
+        private static int ite = 22;
+        private static int tam_clave = 256;
+
+        //Encriptar
+        public static string EncodeHash(string textoCifrar)
+        {
+            try
+            {
+                byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(vector);
+                byte[] saltValueBytes = Encoding.ASCII.GetBytes(valorSalt);
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(textoCifrar);
+
+                PasswordDeriveBytes password =
+                    new PasswordDeriveBytes(palapaso, saltValueBytes,
+                        encrip, ite);
+                byte[] keyBytes = password.GetBytes(tam_clave / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform encryptor =
+                    symmetricKey.CreateEncryptor(keyBytes, InitialVectorBytes);
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream =
+                    new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                byte[] cipherTextBytes = memoryStream.ToArray();
+                memoryStream.Close();
+                cryptoStream.Close();
+                string textoCifradoFinal = Convert.ToBase64String(cipherTextBytes);
+                return textoCifradoFinal;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
